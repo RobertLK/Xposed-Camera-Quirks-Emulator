224aae0fa35c3a607c88068956a8d20dc0e2b7bf
Robert Williams <robert.williams@yoti.com>
2017-10-28 19:18:43 +0100

    Add some more behaviours
    Not hooked in yet, just to start testing more generic
    usage.

---
+++ app/src/main/java/uk/droidcon/com/cameraquirks/xposed/CloseBehaviour.java
@@ -0,0 +1,42 @@
+package uk.droidcon.com.cameraquirks.xposed;
+
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.XposedBridge;
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+
+public enum CloseBehaviour implements Behaviour {
+    DEFAULT(new XC_MethodHook() {
+        @Override
+        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+            XposedBridge.log("Calling super close");
+        }
+    }),
+    EXCEPTION(new XC_MethodReplacement() {
+        @Override
+        protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+            throw new RuntimeException("Xposed blocked camera close");
+        }
+    });
+
+    private XC_MethodHook mHookAction;
+
+    CloseBehaviour(XC_MethodHook action) {
+        mHookAction = action;
+    }
+
+    @Override
+    public void addCamera1Hook(ClassLoader classLoader) {
+        findAndHookMethod("android.hardware.Camera", classLoader, "release", mHookAction);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public void addCamera2Hook(ClassLoader classLoader) {
+        findAndHookMethod("android.hardware.camera2.CameraDevice", classLoader, "close", mHookAction);
+    }
+}
---
+++ app/src/main/java/uk/droidcon/com/cameraquirks/xposed/OrientationBehaviour.java
@@ -0,0 +1,58 @@
+package uk.droidcon.com.cameraquirks.xposed;
+
+import android.hardware.Camera;
+import android.hardware.camera2.CameraCharacteristics;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+
+/**
+ * Override how camera frames are interpreted.
+ */
+public enum OrientationBehaviour implements Behaviour {
+    DEFAULT {
+        @Override
+        int transformOrientation(int original) {
+            return original;
+        }
+    },
+    INVERTED {
+        @Override
+        int transformOrientation(int original) {
+            return original + 180 % 360;
+        }
+    };
+
+    abstract int transformOrientation(int original);
+
+    @Override
+    public void addCamera1Hook(ClassLoader classLoader) {
+        findAndHookMethod("android.hardware.Camera", classLoader, "getCameraInfo", int.class, Camera.CameraInfo.class, new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                Camera.CameraInfo result = (Camera.CameraInfo) param.args[1];
+                XposedBridge.log("Original: " + result.orientation + " transformed: " + transformOrientation(result.orientation));
+                result.orientation = transformOrientation(result.orientation);
+            }
+        });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public void addCamera2Hook(ClassLoader classLoader) {
+        findAndHookMethod("android.hardware.camera2.CameraCharacteristics", classLoader, "get", CameraCharacteristics.Key.class, new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                final CameraCharacteristics.Key key = (CameraCharacteristics.Key) param.args[0];
+                if (CameraCharacteristics.SENSOR_ORIENTATION.equals(key)) {
+                    final Integer resultVal = (Integer) param.getResult();
+                    param.setResult(transformOrientation(resultVal));
+                }
+            }
+        });
+    }
+}
